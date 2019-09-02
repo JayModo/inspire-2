9db6c1e2950283c075a5570e1151d822c34da119
Jeremy O <galaxyjay123@gmail.com>
2019-09-01 19:13:19 -0600

    got-image not displaying

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -5,7 +5,7 @@ const _is = new ImageService()
 
 
 function _drawImg() {
-  let Img = _is.image
+
   document.getElementById('bg-image').innerHTML
   console.log("the image is trying?")
 }
@@ -14,8 +14,9 @@ function _drawImg() {
 //      (you may wish to set it as a background image)
 export default class ImageController {
   constructor() {
+    debugger
     _is.addSubscriber("image", _drawImg)
-    _is.getUrl(_is.image)
+    _is.getUrl(document.getElementById('bg-image').innerHTML)
     _drawImg()
   }
   getUrl() {
--- app/models/image.js
+++ app/models/image.js
@@ -2,10 +2,13 @@ export default class Image {
   constructor(data) {
     this.id = data.id
     this.large_url = data.large_url
-
   }
 
-
+  get Template() {
+    return `
+  <img src="${this.large_url}" alt="">
+    `
+  }
 
 
 
